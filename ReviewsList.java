50916fcd368004474670046eb362c80d5225d1c4
Henryvinhd <henryvinhd@gmail.com>
2019-12-08 15:48:48 -0500

    Add files via upload

---
+++ ReviewsList.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+/**
+ * Write a description of class ReviewsList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ReviewsList
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<Reviews> listOfReviews;
+
+    /**
+     * Constructor for objects of class ReviewsList
+     */
+    public ReviewsList()
+    {
+        listOfReviews = new ArrayList<Reviews>();
+    }
+    
+    public void addReviews(Reviews nReviews){
+        listOfReviews.add(nReviews);
+    }
+    
+    public int size(){
+        return listOfReviews.size();
+    }
+    
+    public Reviews getReview(int i){
+        return listOfReviews.get(i);
+    }
+    
+    
+}
